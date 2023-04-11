c5360d795249466eaa33ae8e21f4a7b585563cfe
Suazo3000 <123909359+Suazo3000@users.noreply.github.com>
2023-04-10 20:18:27 -0500

    Add files via upload

---
+++ ReadmeGen.js
@@ -0,0 +1,37 @@
+class MarkDown {
+    static generateReadme(answers){
+        return `
+    # ${answers.title}
+
+    ## Table of Content
+        - [Project description] (#Description)
+        - [Usage](#Usage)
+        - [Contributing](#Contributing)
+        - [Installation](#Installation)
+        - [Questions](#Questions)
+        - [License](#License)
+
+    ## Description
+    ${answers.description}
+
+    ## Usage
+    ${answers.usage}
+
+    ## Installation
+    ${answers.installation}
+
+    ## Contributing
+    ${answers.contributing}
+
+    ## Questions
+    ${answers.email}
+    ${answers.github}
+
+    ## License 
+    ${answers.license}
+
+    `
+    }
+}
+
+module.exports = MarkDown
